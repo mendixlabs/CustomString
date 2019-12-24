df9dda53686f1a4ffbe9773200c5aa05a4bf0b31
michel-jump <michel@jump.nl>
2019-12-24 11:54:23 +0100

    Replaced deprecated mx.ui.action

--- src/CustomString/widget/CustomString.js
+++ src/CustomString/widget/CustomString.js
@@ -1,22 +1,21 @@
 define([
-    "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
-    "dijit/_TemplatedMixin",
-    "dojo/_base/array",
-    "dojo/_base/lang",
-    "dojo/html",
-    "mxui/dom",
-    "dojo/text!CustomString/widget/template/CustomString.html"
+    'dojo/_base/declare',
+    'mxui/widget/_WidgetBase',
+    'dijit/_TemplatedMixin',
+    'dojo/_base/array',
+    'dojo/_base/lang',
+    'dojo/html',
+    'mxui/dom',
+    'dojo/text!CustomString/widget/template/CustomString.html',
 ], function(declare, _WidgetBase, _TemplatedMixin, dojoArray, lang, html, dom, widgetTemplate) {
-    "use strict";
+    'use strict';
 
-    return declare("CustomString.widget.CustomString", [ _WidgetBase, _TemplatedMixin ], {
-
+    return declare('CustomString.widget.CustomString', [_WidgetBase, _TemplatedMixin], {
         templateString: widgetTemplate,
 
         // Parameters configured in the Modeler.
-        sourceMF: "",
-        renderHTML: "",
+        sourceMF: '',
+        renderHTML: '',
 
         // Internal
         _handles: null,
@@ -28,84 +27,94 @@ define([
         },
 
         postCreate: function() {
-            logger.debug(this.id + ".postCreate");
+            logger.debug(this.id + '.postCreate');
             this._setupEvents();
         },
 
         update: function(obj, callback) {
-            logger.debug(this.id + ".update");
+            logger.debug(this.id + '.update');
             this._contextObj = obj;
 
             if (this._contextObj) {
                 this._resetSubscriptions();
                 this._updateRendering(callback);
-            } else if (this._render){
+            } else if (this._render) {
                 this._render(callback);
             } else {
-                this._executeCallback(callback, "update");
+                this._executeCallback(callback, 'update');
             }
         },
 
         _setupEvents: function() {
-            logger.debug(this.id + "._setupEvents");
-            this.connect(this.customString, "click", function(e) {
+            logger.debug(this.id + '._setupEvents');
+            this.connect(this.customString, 'click', function(e) {
                 // If a microflow has been set execute the microflow on a click.
-                if (this.mfToExecute !== "") {
-                    mx.ui.action(this.mfToExecute, {
-                        params: {
-                            applyto: "selection",
-                            guids: [ this._contextObj.getGuid() ]
+                if (this.mfToExecute !== '') {
+                    mx.data.action(
+                        {
+                            params: {
+                                actionname: this.mfToExecute,
+                                applyto: 'selection',
+                                guids: [this._contextObj.getGuid()],
+                            },
+                            callback: function(obj) {},
+                            error: lang.hitch(this, function(error) {
+                                console.log(
+                                    this.id + ': An error occurred while executing microflow: ' + error.description
+                                );
+                            }),
                         },
-                        callback: function(obj) {},
-                        error: lang.hitch(this, function(error) {
-                            console.log(this.id + ": An error occurred while executing microflow: " + error.description);
-                        })
-                    }, this);
+                        this
+                    );
                 }
             });
         },
 
-        _updateRendering : function (callback) {
-            logger.debug(this.id + "._updateRendering");
-            mx.ui.action(this.sourceMF, {
-                params: {
-                    applyto     : "selection",
-                    guids       : [this._contextObj.getGuid()]
+        _updateRendering: function(callback) {
+            logger.debug(this.id + '._updateRendering');
+            mx.data.action(
+                {
+                    params: {
+                        actionname: this.sourceMF,
+                        applyto: 'selection',
+                        guids: [this._contextObj.getGuid()],
+                    },
+                    callback: lang.hitch(this, this._processSourceMFCallback, callback),
+                    error: lang.hitch(this, function(error) {
+                        alert(error.description);
+                        this._executeCallback(callback, '_updateRendering error');
+                    }),
+                    onValidation: lang.hitch(this, function(validations) {
+                        alert('There were ' + validations.length + ' validation errors');
+                        this._executeCallback(callback, '_updateRendering onValidation');
+                    }),
                 },
-                callback     : lang.hitch(this, this._processSourceMFCallback, callback),
-                error        : lang.hitch(this, function(error) {
-                    alert(error.description);
-                    this._executeCallback(callback, "_updateRendering error");
-                }),
-                onValidation : lang.hitch(this, function(validations) {
-                    alert("There were " + validations.length + " validation errors");
-                    this._executeCallback(callback, "_updateRendering onValidation");
-                })
-            }, this);
+                this
+            );
         },
 
-        _processSourceMFCallback: function (callback, returnedString) {
-            logger.debug(this.id + "._processSourceMFCallback");
+        _processSourceMFCallback: function(callback, returnedString) {
+            logger.debug(this.id + '._processSourceMFCallback');
             if (this.customString) {
                 html.set(this.customString, this.checkString(returnedString, this.renderHTML));
             }
-            this._executeCallback(callback, "_processSourceMFCallback");
+            this._executeCallback(callback, '_processSourceMFCallback');
         },
 
-        checkString : function (string, htmlBool) {
-            logger.debug(this.id + ".checkString");
+        checkString: function(string, htmlBool) {
+            logger.debug(this.id + '.checkString');
             if (!string) {
-                return "";
+                return '';
             }
-            if (string.indexOf("<script") > -1 || !htmlBool) {
-                logger.debug(this.id + ".checkString escape String");
+            if (string.indexOf('<script') > -1 || !htmlBool) {
+                logger.debug(this.id + '.checkString escape String');
                 string = dom.escapeString(string);
             }
             return string;
         },
 
         _resetSubscriptions: function() {
-            logger.debug(this.id + "._resetSubscriptions");
+            logger.debug(this.id + '._resetSubscriptions');
             this.unsubscribeAll();
 
             if (this._contextObj) {
@@ -113,18 +122,18 @@ define([
                     guid: this._contextObj.getGuid(),
                     callback: lang.hitch(this, function(guid) {
                         this._updateRendering();
-                    })
+                    }),
                 });
             }
         },
 
-        _executeCallback: function (cb, from) {
-            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
-            if (cb && typeof cb === "function") {
+        _executeCallback: function(cb, from) {
+            logger.debug(this.id + '._executeCallback' + (from ? ' from ' + from : ''));
+            if (cb && typeof cb === 'function') {
                 cb();
             }
-        }
+        },
     });
 });
 
-require(["CustomString/widget/CustomString"]);
+require(['CustomString/widget/CustomString']);
--- src/CustomString/widget/CustomStringNoContext.js
+++ src/CustomString/widget/CustomStringNoContext.js
@@ -1,45 +1,56 @@
-require([
-    "dojo/_base/declare",
-    "dojo/_base/lang",
-    "CustomString/widget/CustomString"
-], function (declare, lang, _customStringNoContextWidget) {
-
-    return declare("CustomString.widget.CustomStringNoContext", [ _customStringNoContextWidget ], {
-
+require(['dojo/_base/declare', 'dojo/_base/lang', 'CustomString/widget/CustomString'], function(
+    declare,
+    lang,
+    _customStringNoContextWidget
+) {
+    return declare('CustomString.widget.CustomStringNoContext', [_customStringNoContextWidget], {
         postCreate: function() {
-            logger.debug(this.id + ".postCreate");
+            logger.debug(this.id + '.postCreate');
             this._setupEvents();
         },
 
         _setupEvents: function() {
-            logger.debug(this.id + "._setupEvents");
+            logger.debug(this.id + '._setupEvents');
             if (this.mfToExecute) {
-                this.connect(this.customString, "click", this._executeMicroflow);
+                this.connect(this.customString, 'click', this._executeMicroflow);
             }
         },
 
-        _render : function (callback) {
-            logger.debug(this.id + "._render");
-            mx.ui.action(this.sourceMF, {
-                callback     : lang.hitch(this, this._processSourceMFCallback, callback),
-                error        : lang.hitch(this, function(error) {
-                    alert(error.description);
-                    this._executeCallback(callback, "_render error cb");
-                }),
-                onValidation : lang.hitch(this, function(validations) {
-                    alert("There were " + validations.length + " validation errors");
-                    this._executeCallback(callback, "_render onvalidation cb");
-                })
-            }, this);
+        _render: function(callback) {
+            logger.debug(this.id + '._render');
+            mx.data.action(
+                {
+                    params: {
+                        actionname: this.sourceMF,
+                    },
+                    callback: lang.hitch(this, this._processSourceMFCallback, callback),
+                    error: lang.hitch(this, function(error) {
+                        alert(error.description);
+                        this._executeCallback(callback, '_render error cb');
+                    }),
+                    onValidation: lang.hitch(this, function(validations) {
+                        alert('There were ' + validations.length + ' validation errors');
+                        this._executeCallback(callback, '_render onvalidation cb');
+                    }),
+                },
+                this
+            );
         },
 
-        _executeMicroflow: function () {
-            logger.debug(this.id + "._executeMicroflow");
+        _executeMicroflow: function() {
+            logger.debug(this.id + '._executeMicroflow');
             if (this.mfToExecute) {
-                mx.ui.action(this.mfToExecute, {}, this);
+                mx.data.action(
+                    {
+                        params: {
+                            actionname: this.mfToExecute,
+                        },
+                    },
+                    this
+                );
             }
-        }
+        },
     });
 });
 
-require(["CustomString/widget/CustomStringNoContext"]);
+require(['CustomString/widget/CustomStringNoContext']);
